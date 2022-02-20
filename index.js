c5f3bfe5cbeceb6e2e1e1884e2d35f17dfee2e14
Deekshith M D <deekshithmogra@gmail.com>
2022-02-20 23:38:05 +0530

    feat: added dark theme

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
+const dark=document.querySelector(".dark");
 var slide = document.querySelector("#slider");
 var btn1 = document.querySelector("#btn1");
 var btn2 = document.querySelector("#btn2");
 var btn3 = document.querySelector("#btn3");
+
+if(localStorage.getItem("dark")==="dark-theme"){
+    document.body.classList.add("dark-theme")
+    dark.classList.remove("fa-sun")
+    dark.classList.add("fa-moon")
+}
+else{
+    document.body.classList.add("light-theme")
+}
+
+dark.addEventListener('click',()=>{
+    if(dark.classList.contains("fa-sun")){
+        localStorage.clear();
+        localStorage.setItem("dark","dark-theme")
+        dark.classList.remove("fa-sun")
+        dark.classList.add("fa-moon")
+        document.body.classList.remove("light-theme")
+        document.body.classList.add("dark-theme")
+    }
+    else{
+        localStorage.clear();
+        localStorage.setItem("dark","light-theme")
+        dark.classList.remove("fa-moon")
+        dark.classList.add("fa-sun")
+        document.body.classList.remove("dark-theme")
+        document.body.classList.add("light-theme")
+    }
+})
 
 btn1.onclick = function(){
     slide.style.transform = "translateX(0px)";
@@ -26,3 +55,4 @@ btn3.onclick = function(){
     btn1.classList.remove("active");
     btn4.classList.remove("active");
 }
+
